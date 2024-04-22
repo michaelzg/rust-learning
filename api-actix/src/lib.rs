b166b16184bf5c59919586f96f7910382d941bfe
Michael Gao <michaelzgao@gmail.com>
2024-04-21 20:38:44 -0700

    stubbed POST /subscriptions endpoint with tests

--- api-actix/src/lib.rs
+++ api-actix/src/lib.rs
@@ -2,6 +2,12 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct SubscribeForm {
+    name: String,
+    email: String
+}
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("world");
     format!("Hello {}", name)
@@ -11,12 +17,17 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe(_form: web::Form<SubscribeForm>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/greet/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
--- api-actix/tests/health_check.rs
+++ api-actix/tests/health_check.rs
@@ -1,11 +1,20 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let host = "localhost";
+    let listener = TcpListener::bind(format!("{host}:0")).expect("bind failed");
+    let port = listener.local_addr().unwrap().port();
+    let server = api_actix::run(listener).expect("server bringup failed");
+    let _ = tokio::spawn(server);
+    format!("http://{host}:{port}")
+}
+
 #[tokio::test]
 async fn health_check_works() {
-    let port = spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
     let response = client
-        .get(format!("http://localhost:{}/health_check", port))
+        .get(format!("{address}/health_check"))
         .send()
         .await
         .expect("failed request");
@@ -13,10 +22,43 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> u16 {
-    let listener = TcpListener::bind("localhost:0").expect("bind failed");
-    let port = listener.local_addr().unwrap().port();
-    let server = api_actix::run(listener).expect("server bringup failed");
-    let _ = tokio::spawn(server);
-    port
+#[tokio::test]
+async fn subscribe_success() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=bob&email=bob_2%40gmail.com";
+    let response = client
+        .post(format!("{address}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("failed request");
+    assert_eq!(200, response.status().as_u16())
+}
+
+#[tokio::test]
+async fn subscribe_rejected() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let tests = vec![
+        ("missing email", "name=bob"),
+        ("missing name", "email=bob2"),
+        ("missing both email and name", "yo"),
+    ];
+
+    for (desc, body) in tests {
+        let response = client.post(format!("{address}/subscriptions"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("failed request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "{} - failed with body {}", desc, body
+        )
+    }
 }
